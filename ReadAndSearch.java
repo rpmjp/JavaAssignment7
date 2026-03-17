14c79c654623ac190dc78a7fd088b8bfe7943dd1
Robert Jean Pierre <robertjp@live.com>
2026-03-16 21:14:59 -0400

    first commit

--- ReadAndSearch.java
+++ ReadAndSearch.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class ReadAndSearch {
 
@@ -45,11 +46,61 @@ public class ReadAndSearch {
         return result;
     }
 
+    public static String[] addName(String[] arr, String name) {
+        String[] newArr = new String[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        newArr[newArr.length - 1] = name;
+        return newArr;
+    }
+
+    public static int binarySearch(String[] arr, String name) {
+        int leftIndex = 0;
+        int rightIndex = arr.length - 1;
+
+        while (leftIndex <= rightIndex) {
+            int mid = leftIndex + (rightIndex - leftIndex) / 2;
+
+            if (arr[mid].equals(name)) {
+                return mid;
+            } else if (arr[mid].compareTo(name) < 0) {
+                leftIndex = mid + 1;
+            } else {
+                rightIndex = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         writeFile();
         String[] result = readFile();
+
+        System.out.println("This is the data list before adding new names:");
+        for (String name : result) {
+            System.out.println(name);
+        }
+
+        result = addName(result, "Thar");
+        result = addName(result, "Namib");
+
+        System.out.println("\nThis is the data list after adding new names:");
+        for (String name : result) {
+            System.out.println(name);
+        }
+
+        Arrays.sort(result);
+
+        int index = binarySearch(result, "Gobi");
+        System.out.println("\nGobi was found at index: " + index);
+
+        index = binarySearch(result, "Kalahari");
+        System.out.println("\nKalahari was found at index: " + index + "\n");
+
         for (String name : result) {
             System.out.println(name);
         }
     }
+
 }
